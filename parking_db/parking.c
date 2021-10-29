5f703c8b70bd9e715e339ecdf306a7f1f58c2252
Mucura <thefourthpillar@protonmail.com>
2021-10-28 21:14:59 -0400

    Moved the I/O portion out of main
    Placed it in a function, declared an extern var in main

--- parking_db/parking.c
+++ parking_db/parking.c
@@ -7,24 +7,33 @@ struct v_data {
 	char color[10];
 } vehicle;
 
-int main(int argc, char *argv[]) {
+void parking__lot__data(struct v_data);
+
+int main(void) {
+	
+	extern struct v_data vehicle;
+	parking__lot__data(vehicle);
+
+	return EXIT_SUCCESS;
+}
+
+void parking__lot__data(struct v_data machine) {
+	
 	FILE *fp;
-	fp = fopen("parking_data", "a");
+	fp = fopen("lot_data", "a");
 	
 	/* Class */
 	printf("Enter vehicle class:\n-->\t");
-	scanf("%s", vehicle.class);
-	fprintf(fp, "Class: %s\n", vehicle.class);
+	scanf("%s", machine.class);
+	fprintf(fp, "Class: %s\n", machine.class);
 	/* Plate */
 	printf("Enter vehicle plate:\n-->\t");
-	scanf("%s", vehicle.plate);
-	fprintf(fp, "Plate: %s\n", vehicle.plate);
+	scanf("%s", machine.plate);
+	fprintf(fp, "Plate: %s\n", machine.plate);
 	/* Color */
 	printf("Enter vehicle color:\n-->\t");
-	scanf("%s", vehicle.color);
-	fprintf(fp, "Color: %s\n|----------------|\n", vehicle.color);
+	scanf("%s", machine.color);
+	fprintf(fp, "Color: %s\n|----------------|\n", machine.color);
 
 	fclose(fp);
-
-	return EXIT_SUCCESS;
 }
